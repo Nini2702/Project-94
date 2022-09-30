b064f1a466b82089144c39681af6a6107747c79a
Nini2702 <82361495+Nini2702@users.noreply.github.com>
2022-09-30 20:04:46 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDKjsOOV7BW4TugWnxpPtJsbdkfWTkbpfg",
+    authDomain: "project-94-60788.firebaseapp.com",
+    databaseURL: "https://project-94-60788-default-rtdb.firebaseio.com",
+    projectId: "project-94-60788",
+    storageBucket: "project-94-60788.appspot.com",
+    messagingSenderId: "769849771303",
+    appId: "1:769849771303:web:702ee546b0f3d286101502"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
